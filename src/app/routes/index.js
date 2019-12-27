e739faa3ed1ac38121369d23f26910a93ab2804e
AxlEstevez <lacura4ever@gmail.com>
2019-12-27 00:42:17 -0600

    Se actualizo el Archivo de Sign up, ya conecta con la base de datos y almacena, aun hay que hacer manejo de sesiones

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -19,17 +19,29 @@ router.get('/addBook', (req,res) => {
   });
 });
 
-router.get('/Sing_up', (req,res) =>{
-  res.render('Sing_up');
+router.get('/Sign_up', (req,res) =>{
+  res.render('Sign_up');
+});
+
+router.post('/Sign_up',(req,res) =>{
+  var registro = {
+    nombre:  req.body.nombre,
+    apellido_p: req.body.apellido_p,
+    apellido_m: req.body.apellido_m,
+    sexo: req.body.sexo,
+    correo: req.body.correo,
+    usuario: req.body.userName,
+    password: req.body.pass
+  };
+  connection.query("INSERT INTO Usuarios SET ?", registro,
+  (error,results) => {
+    if(error){
+      console.log(error);
+      res.redirect("Sign_up");
+    }
+  });
+  res.render("user_index");
 });
 
 module.exports = router;
 
-/*router.get('/prueba', (req,res) =>{
-  connection.query("SELECT nombre,apellido FROM Autor", (err,result,fields) =>{
-    if(err) throw err;
-    res.render('test',{
-      values: result
-    });
-  });
-});*/
