55cb0c8bf0a1fbb683c77dfc1590db13d99e33b9
AxlEstevez <lacura4ever@gmail.com>
2020-03-09 14:51:50 -0600

    Se agreganron nuevos paquetes para el manejo de sesiones

--- src/app/app.js
+++ src/app/app.js
@@ -24,7 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret:'******',resave:true,saveUninitialized:true}));
+app.use(session({
+  secret:'******',
+  resave:true,
+  saveUninitialized:true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
